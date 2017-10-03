5455e23e49e07b5d8f23c37d4f1b912a503b070a
vk11515 <vikashyadav11515@yahoo.in>
2017-10-04 01:01:24 +0530

    Removing Errors in previous push

--- app/src/main/java/com/example/vikas/financemanager/activity/AccountsActivity.java
+++ app/src/main/java/com/example/vikas/financemanager/activity/AccountsActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.vikas.financemanager.activity;
 
 import android.support.v7.app.AppCompatActivity;
@@ -36,42 +35,3 @@ public class AccountsActivity extends AppCompatActivity {
         tabHost.addTab(t);
     }
 }
-=======
-package com.example.vikas.financemanager.activity;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TabHost;
-
-import com.example.vikas.financemanager.R;
-
-public class AccountsActivity extends AppCompatActivity {
-
-    TabHost tabHost;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accounts);
-        getSupportActionBar().setTitle("Accounts");
-
-
-        tabHost = (TabHost)findViewById(R.id.tabhost);
-        tabHost.setup();
-        //tab1
-        TabHost.TabSpec t=tabHost.newTabSpec("Tab One");
-        t.setIndicator("Bank");
-        t.setContent(R.id.tab1);
-        tabHost.addTab(t);
-        //tab2
-        t=tabHost.newTabSpec("Tab Two");
-        t.setIndicator("Lend-Borrow");
-        t.setContent(R.id.tab2);
-        tabHost.addTab(t);
-        //tab3
-        t=tabHost.newTabSpec("Tab Three");
-        t.setIndicator("Cash");
-        t.setContent(R.id.tab3);
-        tabHost.addTab(t);
-    }
-}
->>>>>>> 9302aad0cb154dfbdda8571cdb55e75dcadcac3c
